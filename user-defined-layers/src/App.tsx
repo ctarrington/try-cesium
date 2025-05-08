de8125e3357546235cbbfa3c59488f6bcf32bcbc
CT Arrington <arrington.ct@gmail.com>
2025-05-07 21:28:27 -0400

    added prettier and nvmrc

--- user-defined-layers/src/App.tsx
+++ user-defined-layers/src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
-import CesiumViewer from "./CesiumViewer.tsx";
+import './App.css';
+import CesiumViewer from './CesiumViewer.tsx';
 
 function App() {
-
   return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            <CesiumViewer />
-        </div>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <CesiumViewer />
+    </div>
   );
 }
 
